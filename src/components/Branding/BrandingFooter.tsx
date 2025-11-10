403b2acb8490d17a74472100787cb4986b95cf0d
Francisco Suarez <fjsuarez@gmail.com>
2025-11-11 00:04:42 +0100

    Update logos

--- src/components/Branding/BrandingFooter.tsx
+++ src/components/Branding/BrandingFooter.tsx
@@ -6,24 +6,6 @@ export default function BrandingFooter() {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white/95 backdrop-blur-sm border-t-2 shadow-lg z-30" style={{ borderTopColor: '#6DC201' }}>
       <div className="flex items-center justify-center gap-8 px-4 py-2 md:py-3">
-        {/* IE Logo */}
-        <div className="flex items-center gap-2">
-          <div className="relative w-8 h-8 md:w-10 md:h-10">
-            <Image
-              src="/IE.svg"
-              alt="IE University"
-              fill
-              className="object-contain"
-            />
-          </div>
-          <span className="hidden sm:inline text-xs md:text-sm text-gray-600 font-medium">
-            IE University
-          </span>
-        </div>
-
-        {/* Separator */}
-        <div className="h-6 md:h-8 w-px bg-gray-300" />
-
         {/* VIG Logo */}
         <div className="flex items-center gap-2">
           <div className="relative w-16 h-8 md:w-20 md:h-10">
@@ -34,9 +16,21 @@ export default function BrandingFooter() {
               className="object-contain"
             />
           </div>
-          <span className="hidden sm:inline text-xs md:text-sm text-gray-600 font-medium">
-            Vienna Insurance Group
-          </span>
+        </div>
+
+        {/* Separator */}
+        <div className="h-6 md:h-8 w-px bg-gray-300" />
+
+        {/* IE Logo */}
+        <div className="flex items-center gap-2">
+          <div className="relative w-24 h-10 md:w-32 md:h-12">
+            <Image
+              src="/IE.svg"
+              alt="IE University"
+              fill
+              className="object-contain"
+            />
+          </div>
         </div>
       </div>
 
